95e10a9d4d21e35eec6216f5827e7f399ab9c989
Kaloyan Sotirov <kaloyansotirov05@gmail.com>
2023-09-28 11:26:32 +0300

    first spring boot commit

---
+++ src/main/java/com/example/diplomnabe/DiplomnaBeApplication.java
@@ -0,0 +1,14 @@
+package com.example.diplomnabe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiplomnaBeApplication {
+
+    public static void main(String[] args)
+    {
+        SpringApplication.run(DiplomnaBeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/diplomnabe/DiplomnaBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.diplomnabe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiplomnaBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
